5d4210c579961243eb11009b52a7cfaa31ab687c
Ed Basurto <ed@basurto.org>
2017-11-11 12:35:32 -0800

    Fixed small bug

--- src/ScheduleMaker.java
+++ src/ScheduleMaker.java
@@ -11,7 +11,7 @@ import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.s s.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
@@ -71,14 +71,14 @@ public class ScheduleMaker {
       String userInputStr;
       int userInputInt;
       
-      //Course[] courses;
+      Course[] courses1;
       
       
       // enter course info
       System.out.println("How many courses would you like to take?");
       userInputStr = scanner.nextLine();
       userInputInt = Integer.parseInt(userInputStr);
-      courses = new Course[userInputInt];
+      courses1 = new Course[userInputInt];
       
       for (int i = 0; i < userInputInt; i++) {
          System.out.println("Enter the class: ");
