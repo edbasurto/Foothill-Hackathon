674d6b74a80b871715e7f8d4363dda0f495d263d
Ed Basurto <ed@basurto.org>
2017-11-11 12:26:56 -0800

    API added

--- src/ScheduleMaker.java
+++ src/ScheduleMaker.java
@@ -11,7 +11,7 @@ import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.s s.usermodel.Workbook;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
@@ -71,7 +71,7 @@ public class ScheduleMaker {
       String userInputStr;
       int userInputInt;
       
-      Course[] courses;
+      //Course[] courses;
       
       
       // enter course info
