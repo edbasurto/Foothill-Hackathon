990f84c2f1be22edca4d3a1294774a7fb75ce1cd
Ed Basurto <ed@basurto.org>
2017-11-11 18:17:43 -0800

    Added prompt

--- src/ScheduleMaker.java
+++ src/ScheduleMaker.java
@@ -3,35 +3,40 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class ScheduleMaker {
 	
 	//This is our project
+   private static ArrayList<String> courseNames = new ArrayList<String>();
+   private ArrayList<String[][]> courses = new ArrayList<String[][]>();
    
-   public static Scanner scanner;
-   private ArrayList<String> courseNames = new ArrayList<String>();
-   private ArrayList<HSSFCell[][]> courses = new ArrayList<HSSFCell[][]>();
-   
-   private void getCoursesAva() {
+private static void getCoursesAva() {
 	   try {
 		    File file = new File("C:/coursecatalog.xlsx");
 		    POIFSFileSystem fs = new POIFSFileSystem(new FileInputStream(file));
-		    HSSFWorkbook wb = new HSSFWorkbook(fs);
+		    XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream(file));
 		    for(int s = 0; s < wb.getNumberOfSheets(); s++) {
-		    	HSSFSheet sheet = wb.getSheetAt(s);
-			    HSSFRow row;
-			    HSSFCell cell;
+		    	XSSFSheet sheet = wb.getSheetAt(s);
+			    XSSFRow row;
+			    XSSFCell cell;
+			    
+			    DataFormatter df = new DataFormatter();
 			    
 			    courseNames.add(sheet.getSheetName());
 
@@ -50,14 +55,14 @@ public class ScheduleMaker {
 			        }
 			    }
 			    
-			    HSSFCell[][] temp = new HSSFCell[rows][cols];
+			    String[][] temp = new String[rows][cols];
 			    for(int r = 0; r < rows; r++) {
 			        row = sheet.getRow(r);
 			        if(row != null) {
 			            for(int c = 0; c < cols; c++) {
 			                cell = row.getCell((short)c);
 			                if(cell != null) {
-			                	temp[r][c] = cell;
+			                	temp[r][c] = df.formatCellValue(cell);
 			                }
 			            }
 			        }
@@ -69,24 +74,78 @@ public class ScheduleMaker {
    }
    
    public static void main(String[] args) {
-      scanner = new Scanner(System.in);
-      String userInputStr;
-      int userInputInt;
-      
-      Course[] courses1;
+	   
+	   getCoursesAva();
+	   
+	  int numClasses = 0;
+      Scanner scan = new Scanner(System.in);
       
       
       // enter course info
       System.out.println("How many courses would you like to take?");
-      userInputStr = scanner.nextLine();
-      userInputInt = Integer.parseInt(userInputStr);
-      courses1 = new Course[userInputInt];
-      
-      for (int i = 0; i < userInputInt; i++) {
-         System.out.println("Enter the class: ");
-         //courses[i].setName(scanner.nextLine());
+      numClasses = scan.nextInt();
+      String[] classNames = new String[numClasses];
+      int[] classNums = new int[numClasses];
+      char[] classLevels = new char[numClasses];
+      boolean[] classHonors = new boolean[numClasses]; 
+      for (int i = 0; i < numClasses; i++) {
+    	 boolean error = true;
+         while(error) {
+        	 System.out.println("Enter the class name (4 character format): "); // Math 1B
+             String temp = scan.nextLine();
+             if(Course.checkName(temp)) {
+            	 classNames[i] = temp;
+            	 error = false;
+             } else {
+            	 System.out.println("Please enter a valid course name");
+             }
+         }
+         error=true;
+         while(error) {
+        	 int temp = 0;
+             System.out.println("Enter num level: ");
+             try {
+            	 temp = Integer.parseInt(scan.nextLine());
+            	 if(Course.checkNum(temp)) {
+            		 classNums[i] = temp;
+            		 error = false;
+            	 }
+             } catch (Exception e) {
+            	 System.out.println("Please enter a valid number between 1 and 999");
+             }
+         }
+         error = true;
+         while(error) {
+        	 String line = "";
+        	 System.out.println("Enter the class level ['.' if N/A]: ");
+        	 line = scan.nextLine();
+        	 if(line.length() == 1) {
+        		 if(Course.checkLevel(line.charAt(0))) {
+        			 classLevels[i] = line.charAt(0);
+        			 error = false;
+        		 }
+        	 }
+         }
+         error = true;
+         while(error) {
+        	 String line = "";
+        	 System.out.println("Is it an Honors class? [y/n]:  ");
+        	 line = scan.nextLine();
+        	 if(line.length() == 1) {
+        		 if(Character.toUpperCase(line.charAt(0)) == 'Y') {
+        			 classHonors[i] = true;
+        			 error = false;
+        		 } else if(Character.toUpperCase(line.charAt(0)) == 'N') {
+        			 classHonors[i] = false;
+        			 error = false;
+        		 } else {
+            		 System.out.println("Please enter either Y or N");
+            	 }
+        	 } else {
+        		 System.out.println("Please enter either Y or N");
+        	 }
+         }
       }
-      
    }
 }
 
@@ -149,7 +208,7 @@ class Course{
    public char getLvl() { return cLvl; }
    public double getNumUnits() { return numUnits; }
    
-   private static boolean checkName(String courseName) {
+   public static boolean checkName(String courseName) {
 	   int len = courseName.length();
 	   if(len < 3 || len > 4) {
 		   return false;
@@ -165,20 +224,20 @@ class Course{
 	   return true;
    }
    
-   private static boolean checkSchool(char cSchl) {
+   public static boolean checkSchool(char cSchl) {
 	   return cSchl == 'F' || cSchl == 'D';
    }
    
-   private static boolean checkNum(int num) {
+   public static boolean checkNum(int num) {
 	   return num > 0 && num < 1000; // Course number needs to be 3 digits EX: 005, 057, 500
    }
    
-   private static boolean checkLevel(char lvl) {
+   public static boolean checkLevel(char lvl) {
 	   return lvl == '.' || Character.toUpperCase(lvl) == 'A' || Character.toUpperCase(lvl) == 'B' ||
 			   Character.toUpperCase(lvl) == 'C' || Character.toUpperCase(lvl) == 'D';
    }
    
-   private static boolean checkUnits(int numUnits) {
+   public static boolean checkUnits(int numUnits) {
 	   return numUnits > 1 || numUnits < 6;
    }
 }
